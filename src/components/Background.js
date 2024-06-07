582d3f2e4b1e42ad1bb29c6c2072e92a45da7c4a
CameronIFrazier <taterfrazier1@gmail.com>
2024-06-06 19:06:42 -0700

    wip

---
+++ src/components/Background.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Background = ({ children}) => {
+    return(
+        <div style={ {backgroundColor : 'white', width: '100%', height: '100vh'}}>
+
+        </div>
+    )
+}
+
+export default Background;
---
+++ src/components/MiddleContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MiddleContainer =  ({w, h}) => {
+    return(
+        <div style = {{width: w, height: h, backgroundColor: 'black'}}>
+        </div> 
+
+    )
+}
+
+export default MiddleContainer;
